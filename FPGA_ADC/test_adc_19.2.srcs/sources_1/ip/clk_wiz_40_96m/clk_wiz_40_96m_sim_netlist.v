817804a5d9bd1a635d8548433eb4e71187cdfacc
YuDengxiang <3570221294@qq.com>
2026-07-03 20:56:09 +0800

    接入ADC_RATE正常工作

--- FPGA_ADC/test_adc_19.2.srcs/sources_1/ip/clk_wiz_40_96m/clk_wiz_40_96m_sim_netlist.v
+++ FPGA_ADC/test_adc_19.2.srcs/sources_1/ip/clk_wiz_40_96m/clk_wiz_40_96m_sim_netlist.v
@@ -3,8 +3,8 @@
 // Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
 // Date        : Thu May 14 12:35:07 2026
 // Host        : nine running 64-bit major release  (build 9200)
-// Command     : write_verilog -force -mode funcsim
-//               C:/FPGA_project/xc7z020clg484-1/test_adc_19.2/test_adc_19.2.srcs/sources_1/ip/clk_wiz_40_96m/clk_wiz_40_96m_sim_netlist.v
+// Command     : write_verilog -force -mode funcsim -rename_top clk_wiz_40_96m -prefix
+//               clk_wiz_40_96m_ clk_wiz_40_96m_sim_netlist.v
 // Design      : clk_wiz_40_96m
 // Purpose     : This verilog netlist is a functional simulation representation of the design and should not be modified
 //               or synthesized. This netlist cannot be used for SDF annotated simulation.
@@ -35,7 +35,6 @@ module clk_wiz_40_96m
         .resetn(resetn));
 endmodule
 
-(* ORIG_REF_NAME = "clk_wiz_40_96m_clk_wiz" *) 
 module clk_wiz_40_96m_clk_wiz_40_96m_clk_wiz
    (clk_out1,
     resetn,
